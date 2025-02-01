08defbb96c4d29e301ba813ef265dfa7e329ba52
Jojohere05 <2005jhotikaraja@gmail.com>
2025-02-01 20:02:04 +0530

    Fibonocci calculator.java

--- Calculator.java
+++ Calculator.java
@@ -17,4 +17,22 @@ class Calculator{
 			return 0;
 		}
 	}
+	public int fibonocci(int n){
+		int n1=0,n2=1,n3;
+		if (n==1){
+			return n1
+		}
+		else if (n==2){
+			System.out.print(n1+" "+n2);
+		}
+		else{
+			System.out.println(n1+" "+n2);
+			for (int i=2;i<n;i++){
+				n3=n1+n2;
+				System.out.println(" "+n3);
+				n1=n2;
+				n2=n3;
+			}
+		}
+	}
 }
